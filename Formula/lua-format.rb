392ed19a3bdb4596e0f451f5ddafca2d552db241
Saad Parwaiz <saad.parwaiz1@gmail.com>
2021-05-11 16:34:13 +0100

    new formula lua-format

---
+++ Formula/lua-format.rb
@@ -0,0 +1,21 @@
+class LuaFormat < Formula
+  desc "Formatter For Lua"
+  homepage "https://github.com/Koihik/LuaFormatter"
+  url "https://github.com/Koihik/LuaFormatter.git",
+    tag:      "1.3.5",
+    revision: "638ec8a7c114a0082ce60481afe8f46072e427e5"
+  license "MIT"
+  head "https://github.com/Koihik/LuaFormatter.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "."
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    true
+  end
+end
