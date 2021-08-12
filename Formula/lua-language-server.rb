b15937058e226ca09c42a61936c078533aba5da2
saadparwaiz1 <73385353+saadparwaiz1@users.noreply.github.com>
2021-08-12 23:48:04 +0000

    lua-language-server: update 2.3.6 bottle.

--- Formula/lua-language-server.rb
+++ Formula/lua-language-server.rb
@@ -8,9 +8,9 @@ class LuaLanguageServer < Formula
   head "https://github.com/sumneko/lua-language-server.git"
 
   bottle do
-    root_url "https://github.com/saadparwaiz1/homebrew-personal/releases/download/lua-language-server-2.3.4"
-    sha256 cellar: :any,                 catalina:     "755d6b43406e6cdaa34846cc529bd85714c8d896d478d8062658e1bf1526bbbf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ea334e35616059a70e57ca7c144209c864fa03bd8a2e5f07c19b2bb95361bf74"
+    root_url "https://github.com/saadparwaiz1/homebrew-personal/releases/download/lua-language-server-2.3.6"
+    sha256 cellar: :any,                 catalina:     "8f9fdb5a474a129d6893421dad1a11708ae6646d7117e3624e3a45cd3fdb20f7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5633856ea37cc3cdfba0b013dc2ac61eff73a3a636729dff58511fc65b69a7a7"
   end
 
   depends_on "ninja" => :build
